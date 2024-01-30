2ef0c95cd922fac1ba4335ffc8c9f7c77b26c759
Stanislav Bogdanov <stanislav.tiryoshin@gmail.com>
2024-01-30 21:41:29 +0600

    redux toolkit example slice

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import Buttons from "./components/Buttons";
 import { useSelector } from "react-redux";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
+import { setIsEven, setIsOdd } from "./redux/slices/isEvenSlice";
 
 function App() {
   const status = useSelector((state) => state.status);
   const counter = useSelector((state) => state.counter);
+  const isEven = useSelector((state) => state.isEven);
 
   const dispatch = useDispatch();
 
@@ -19,12 +21,23 @@ function App() {
     }
   }, [counter]);
 
+  useEffect(() => {
+    if (counter % 2 === 0) {
+      dispatch(setIsEven());
+    } else {
+      dispatch(setIsOdd());
+    }
+  }, [counter]);
+
+  console.log(isEven);
+
   return (
     <>
       <h1>State Management in React</h1>
       <Display />
       <Buttons />
       <div>Counter value is {status}</div>
+      <div>{isEven.isEven ? <h3>Even</h3> : <h3>Odd</h3>}</div>
     </>
   );
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,15 @@
-import { createStore } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./reducers/rootReducer";
+import counterReducer from "./reducers/counterReducer";
+import statusReducer from "./reducers/statusReducer";
+import isEvenReducer from "./slices/isEvenSlice";
 
-const store = createStore(rootReducer);
+const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+    status: statusReducer,
+    isEven: isEvenReducer,
+  },
+});
 
 export default store;
